4741598e594509d0006c30d35d043fcfd327d705
Mariah <alinemariah@hotmail.com>
2025-07-09 14:17:20 +0200

    Initial commit

---
+++ src/components/test.jsx
@@ -0,0 +1,11 @@
+const Test = () => {
+    return ( 
+        <div>
+            <h1 className="bg-green-300 font-roboto text-8xl">
+                Tantz der Kulturen!
+            </h1>
+        </div>
+     );
+}
+ 
+export default Test;
