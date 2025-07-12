072fff20155c6561a273deef120702ec13c056a7
Mariah <alinemariah@hotmail.com>
2025-07-12 19:47:39 +0200

    ref: use flowbite for menu dropdown

--- src/components/Dropdowns/DropdownMenu.jsx
+++ src/components/Dropdowns/DropdownMenu.jsx
@@ -1,54 +1,19 @@
-import {  useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { Dropdown, DropdownItem } from "flowbite-react";
 
-const DropdownMenu = ({ section, items, isOpen, onOpen, onClose }) => {
-    const dropdownRef = useRef(null);
-
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        onClose();
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, [onClose]);
-
-
+const DropdownMenu = ({ section, items }) => {
     return ( 
-        <nav className="relative" ref={dropdownRef}>
-            <button
-            onClick={onOpen}
-            className="text-gray-700 hover:text-teal-600 flex items-center gap-1"
-            >
-                {section}
-                <ChevronDownIcon className="w-4 h-4 mt-1" />
-            </button>
-
-            {isOpen && (
-                <div className="absolute top-10 left-0 w-56 bg-white shadow-lg rounded-md ring-1 ring-black ring-opacity-5 z-10">
-                    <div className="py-2">
-                    {items.map((item) => (
-                        <Link
-                        key={item.name}
-                        to={item.path}
-                        onClick={() => {
-                            if (typeof onClose === 'function') {
-                              onClose(); // fecha o menu ao clicar
-                            }
-                          }}
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        >
-                            {item.name}
-                        </Link>
-                    ))}
-                    </div>  
-                </div>
-            )}
-        </nav>
+        <Dropdown label={section} className="text-gray-700 hover:text-teal shadow-lg rounded-md ring-1 ring-black ring-opacity-5  bg-white hover:bg-gray-100">
+          {items.map((item) => (
+              <DropdownItem as={Link}
+                to={item.path}
+                key={item.name}
+                className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              >
+                {item.name}
+              </DropdownItem>
+          ))}
+        </Dropdown>
      );
 }
  
