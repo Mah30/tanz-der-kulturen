115f009e95d60e3b626a17a3057f1c089bb41ab9
Mariah <alinemariah@hotmail.com>
2025-09-09 17:35:01 +0200

    feat: update menuItems, common.json en and common.json de

--- src/menuItems.js
+++ src/menuItems.js
@@ -1,47 +1,119 @@
- const menuItems = (t) => [
+const menuItems = (t) => [
   {
     key: "home",
     title: t("menu.home"),
-    url: '/',
+    url: "/",
   },
   {
     key: "tanzevents",
-    title: "Tanzevents", // Level 0
+    title: t("menu.tanzevents"),
     submenu: [
-      {key: "tanzworkshops", title: "Tanzworkshops", url:"/tanzevent/tanzworkshops"},
-      {key: "performances", title: "Performances", url:"/tanzevent/performances"}
+      {
+        key: "tanzworkshops",
+        title: t("menu.tanzworkshops"),
+        url: "/tanzevent/tanzworkshops",
+      },
+      {
+        key: "performances",
+        title: t("menu.performances"),
+        url: "/tanzevent/performances",
+      },
+      {
+        key: "tanzreise",
+        title: t("menu.tanzreise"),
+        url: "/tanzevent/tanzreise",
+      },
     ],
   },
   {
     key: "weiterbildung",
-    title: "Weiterbildung",
+    title: t("menu.weiterbildung"),
     submenu: [
-      { key: "aufbau-termine", title: "Aufbau + Termine", url: "/weiterbildung/aufbau-termine" },
-      { key: "konzept", title: "Konzept", url: "/weiterbildung/konzept" },
-      { key: "buch", title: "Buch", url: "/weiterbildung/buch" },
-      { key: "lernplattform", title: "Lernplattform", url: "/weiterbildung/lernplattform" },
-      { key: "anmeldung-weiterbildung", title: "Anmeldung", url: "/weiterbildung/anmeldung" },
+      {
+        key: "aufbau-termine",
+        title: t("menu.aufbau-termine"),
+        url: "/weiterbildung/aufbau-termine",
+      },
+      {
+        key: "konzept",
+        title: t("menu.konzept"),
+        url: "/weiterbildung/konzept",
+      },
+      {
+        key: "buch",
+        title: t("menu.buch"),
+        url: "/weiterbildung/buch",
+      },
+      {
+        key: "lernplattform",
+        title: t("menu.lernplattform"),
+        url: "/weiterbildung/lernplattform",
+      },
+      {
+        key: "anmeldung-weiterbildung",
+        title: t("menu.anmeldung-weiterbildung"),
+        url: "/weiterbildung/anmeldung",
+      },
     ],
   },
   {
     key: "uber-uns",
-    title: "Über uns",
+    title: t("menu.uber-uns"),
     submenu: [
-      { key: "wer-wir-sind", title: "Wer wir sind", url: "/uber-uns/team" },
-      { key: "testimonials", title: "Testimonials", url: "/uber-uns/testimonials" },
-      { key: "team", title: "Team", url: "/uber-uns/team" },
+      {
+        key: "unser-werte",
+        title: t("menu.unser-werte"),
+        url: "/uber-uns/unser-werte",
+      },
+      {
+        key: "unser-team",
+        title: t("menu.unser-team"),
+        url: "/uber-uns/unser-team",
+      },
+      {
+        key: "vergangene-projekte",
+        title: t("menu.vergangene-projekte"),
+        url: "/uber-uns/vergangene-projekte",
+      },
+      {
+        key: "testimonials",
+        title: t("menu.testimonials"),
+        url: "/uber-uns/testimonials",
+      },
+      
     ],
   },
   {
     key: "kontakt",
-    title: "Kontakt",
+    title: t("menu.kontakt"),
     submenu: [
-      { key: "anmeldung-kontakt", title: "Anmeldung", url: "/kontakt/anmeldung" },
-      { key: "mitgliedschaft", title: "Mitgliedschaft", url: "/kontakt/mitgliedschaft" },
-      { key: "spenden", title: "Spenden", url: "/kontakt/spenden" },
+      {
+        key: "so-erreichst-du-uns",
+        title: t("menu.so-erreichst-du-uns"),
+        url: "/kontakt/so-erreichst-du-uns",
+      },
+      {
+        key: "mitglied-werden",
+        title: t("menu.mitglied-werden"),
+        url: "/kontakt/mitglied-werden",
+      },
+      {
+        key: "spenden",
+        title: t("menu.spenden"),
+        url: "/kontakt/spenden",
+      },
+      {
+        key: "open-call",
+        title: t("menu.open-call"),
+        url: "/kontakt/open-call",
+      },
+      {
+        key: "kooperation",
+        title: t("menu.kooperation"),
+        url: "/kontakt/kooperation",
+      },
     ],
   },
 ];
-
 
 export default menuItems;
