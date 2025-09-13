d0262cb7a0263a7d66158ba51aed6f809ff811f6
erua imoudu <84855713+imouduerua@users.noreply.github.com>
2025-09-13 06:48:28 -0400

    Delete admin directory

--- admin/components/main/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-white p-3 text-center">
-      <small>© 2023 Afrikanischer Tanz. All rights reserved.</small>
-    </footer>
-  );
-}
