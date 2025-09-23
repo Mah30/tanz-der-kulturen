664e39d2db55ad1250d392610b74fe807056c2af
maheklalwani23-ui <maheklalwani23@gmail.com>
2025-09-23 14:20:15 +0200

    Update App.jsx
    feat(routes): add /registration route (alias of /signup)

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Contact from "./pages/Kontakt";
 import TanzworkshopsPage from "./pages/Tanzevents/TanzworkshopsPage";
 /* import LoginPage from "./pages/LoginPage"; */
 import Performances from "./pages/Tanzevents/Performances";
-/* import SignupPage from "./pages/SignupPage" */
 import Footer from "./components/Footer";
+import SignupPage from "./pages/Signup";
 
 
 
@@ -22,16 +22,14 @@ function App() {
    
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/tanzevent/tanzworkshops" element={<TanzworkshopsPage />} />
-        <Route path="/tanzevent/Performances" element={<Performances />} />
+  <Route path="/" element={<Home />} />
+  <Route path="/contact" element={<Contact />} />
+  <Route path="/tanzevent/tanzworkshops" element={<TanzworkshopsPage />} />
+  <Route path="/tanzevent/Performances" element={<Performances />} />
+<Route path="/registration" element={<SignupPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
-       {/*  <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} /> */}
-      </Routes>
-    </div>
+</Routes>
     
   );
 }
