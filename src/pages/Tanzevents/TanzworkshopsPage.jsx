60a73640ea7e2b559d440012d124c4152a01d1ce
Mariah <alinemariah@hotmail.com>
2025-09-23 19:31:55 +0200

    tanzworkshopactualizations

--- src/pages/Tanzevents/TanzworkshopsPage.jsx
+++ src/pages/Tanzevents/TanzworkshopsPage.jsx
@@ -8,13 +8,13 @@ const TanzworkshopsPage = () => {
     {
       img: "/src/assets/images/imgTanzworkshopPage/mega_Gruppe_dabei.jpg",
       title: "Tanzworkshop mit Livemusik vom 15. - 17. August in Hamburg",
-      desc: "Thema: Tanz der Befreiung mit Stephanie Bangoura und Massa Dembelé. Im Tanz der Befreiung finden wir einen Zugang zu der Kraft unserer Aggressionen. Aggressionen helfen uns, Entscheidungen zu fällen und nicht gewollte Verbindungen abzuschneiden. Der Mythos der Befreiung lehrt uns, unserer Intuition zu folgen. Folgen wir ihr, braucht es keine Diskussion, keine Rechtfertigungen oder Erklärungen. Im Tanz der Befreiung lernen wir, wie der Wind zu verschwinden, aufzutauchen und selbstbestimmt unseren ureigenen Weg einzuschlagen. Es wird aufgezeigt, wie die Prinzipien von animistischen Tanztraditionen zur Heilung und Gruppenbildung sinnvoll in jeden Bewegungsunterricht übersetzt werden können. Alle Körper und tänzerischen Erfahrungslevel sind willkommen.",
+      desc: "Thema: Tanz der Befreiung mit Stephanie Bangoura und Massa Dembelé. Im Tanz der Befreiung finden wir einen Zugang zu der Kraft unserer Aggressionen. Aggressionen helfen uns, Entscheidungen zu fällen und nicht gewollte Verbindungen abzuschneiden. Der Mythos der Befreiung lehrt uns, unserer Intuition zu folgen. Folgen wir ihr, braucht es keine Diskussion, keine Rechtfertigungen oder Erklärungen.",
     },
     {
       img: "/src/assets/images/imgTanzworkshopPage/afrika_tanztherapie_hamburg.jpg",
       title:
         "Tanzworkshop mit Livemusik vom 12. – 14. September 2025 in Hamburg",
-      desc: "Thema: Tanz der Befreiung mit Stephanie Bangoura und Massa Dembelé. Im Tanz der Befreiung finden wir einen Zugang zu der Kraft unserer Aggressionen. Aggressionen helfen uns, Entscheidungen zu fällen und nicht gewollte Verbindungen abzuschneiden. Der Mythos der Befreiung lehrt uns, unserer Intuition zu folgen. Folgen wir ihr, braucht es keine Diskussion, keine Rechtfertigungen oder Erklärungen. Im Tanz der Befreiung lernen wir, wie der Wind zu verschwinden, aufzutauchen und selbstbestimmt unseren ureigenen Weg einzuschlagen. Es wird aufgezeigt, wie die Prinzipien von animistischen Tanztraditionen zur Heilung und Gruppenbildung sinnvoll in jeden Bewegungsunterricht übersetzt werden können. Alle Körper und tänzerischen Erfahrungslevel sind willkommen.",
+      desc: "Thema: Tanz der Befreiung mit Stephanie Bangoura und Massa Dembelé. Im Tanz der Befreiung finden wir einen Zugang zu der Kraft unserer Aggressionen. Aggressionen helfen uns, Entscheidungen zu fällen und nicht gewollte Verbindungen abzuschneiden. Der Mythos der Befreiung lehrt uns, unserer Intuition zu folgen. Folgen wir ihr, braucht es keine Diskussion, keine Rechtfertigungen oder Erklärungen.",
     },
     {
       img: "/src/assets/images/imgTanzworkshopPage/test.jpg",
@@ -37,14 +37,14 @@ const TanzworkshopsPage = () => {
       img: "/src/assets/images/imgTanzworkshopPage/tanzworkshop_afrika.jpg",
       title:
         "Tanzworkshop mit Livemusik vom 14. - 16. November 2025 in Hamburg",
-      desc: "Thema: Tanz der Schlange mit Stephanie Bangoura und Massa Dembelé. Im Tanz der Schlange verbinden wir uns mit dem zentralen Schöpfungsmythos vieler indigener Traditionen: Die Schlange langweilte sich im Himmel, weil sie keinen Widerstand erfuhr. Deshalb stieg sie eines Tages als Regenbogen zur Erde hinab. So entstand die Verbindung zwischen Himmel und Erde, zwischen geistiger und materieller Welt. Im permanenten Schlängeln verbindet und versöhnt die Schlange die Gegensätze. Es heißt, seitdem sind Menschen und Gottheiten für immer Eins. Freue Dich auf ein Tanzerlebnis, in dem Du von der Gruppe und der kraftvollen Livemusik getragen wirst. In diesem Live-Erlebnis kannst Du Dich spüren, ausdrücken und loslassen. Du lernst einfache Bewegungen aus dem westafrikanischen Tanz, mit denen Du mehr in Deine Erdung und Aufrichtung kommst.",
+      desc: "Thema: Tanz der Schlange mit Stephanie Bangoura und Massa Dembelé. Im Tanz der Schlange verbinden wir uns mit dem zentralen Schöpfungsmythos vieler indigener Traditionen: Die Schlange langweilte sich im Himmel, weil sie keinen Widerstand erfuhr. Deshalb stieg sie eines Tages als Regenbogen zur Erde hinab. So entstand die Verbindung zwischen Himmel und Erde, zwischen geistiger und materieller Welt.",
     },
   ];
 
   return (
-    <main>
+    <main className="mt-16">
       <section className="max-w-4xl mx-auto text-center mb-16">
-        <h1 className="text-2xl font-bold">Tanz-Workshops</h1>
+        <h1 className="text-4xl font-bold">Tanz-Workshops</h1>
         <h3 className="text-lg font-medium">
           Lerne Dich tanzend mit Live-Musik kennen!
         </h3>
@@ -54,9 +54,45 @@ const TanzworkshopsPage = () => {
         </p>
       </section>
 
-      <section>
+      <section className="mb-16">
         <h2 className="text-center text-2xl font-semibold mb-10">
-          Unsere kommenden Rituellen Tanz-Workshops mit Live-Musik
+          Alle Hamburg-Events
+        </h2>
+
+        <div className="flex flex-col md:flex-row">
+          {/* Event 1 */}
+          <div className="relative w-full md:w-1/2 h-80">
+            <img
+              src="/src/assets/images/imgTanzworkshopPage/test.jpg"
+              alt="Hamburg Event 1"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 flex items-end pb-4 justify-center">
+              <h3 className="text-xl font-bold text-center">
+                Alle kommenden Freitag-Events in Hamburg
+              </h3>
+            </div>
+          </div>
+
+          {/* Event 2 */}
+          <div className="relative w-full md:w-1/2 h-80">
+            <img
+              src="/src/assets/images/imgTanzworkshopPage/test.jpg"
+              alt="Hamburg Event 2"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 flex items-end pb-4 justify-center">
+              <h3 className="text-xl font-bold text-center">
+                Alle kommenden Wochenend-Events in Hamburg
+              </h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h2 className="text-center text-2xl font-semibold mb-10 mt-16">
+          Alle kommenden Rituellen Tanz-Workshops mit Live-Musik weltweit
         </h2>
 
         <div className="w-full flex flex-col">
@@ -74,7 +110,7 @@ const TanzworkshopsPage = () => {
                   className="w-full object-contain object-center"
                 />
               </div>
-              <div className="w-full md:w-1/2 p-4">
+              <div className="w-full md:w-1/2 p-8 flex flex-col justify-evenly">
                 <h3 className="text-2xl font-semibold mb-4 text-center">
                   {workshop.title}
                 </h3>
